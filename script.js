79d8e46b934f34a6f0db1d9c7e93429355e49721
ojas-ba <148962749+ojas-ba@users.noreply.github.com>
2024-01-06 09:24:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function toggleMenu() {
+function menu_open() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
